3636c2f31d065320252e686072eb2fe5412a36ec
IHacksawRidge <106923539+IHacksawRidge@users.noreply.github.com>
2022-11-04 13:40:54 +0200

    Add files via upload

---
+++ misc/misc.py
@@ -0,0 +1,21 @@
+import dataclasses
+from random import randint
+import pygame as pg
+
+from settings import CELL_SIZE, GAME_WIDTH, GAME_HEIGHT
+
+
+@dataclasses.dataclass
+class Coord:
+    x: int
+    y: int
+
+
+@dataclasses.dataclass
+class Direction:
+    key: tuple[pg.key, ...]
+    velocity: tuple[int, int]
+
+
+def get_random_coord() -> Coord:
+    return Coord(randint(0, GAME_WIDTH - 1) * CELL_SIZE, randint(0, GAME_HEIGHT - 1) * CELL_SIZE)
